914d1cb05a6ccbc230e078f3399930f2d8637730
mscoots <Matt.Scott@unh.edu>
2022-07-12 17:08:03 -0400

    added location of brook trout

--- Knotweed Mapping.R
+++ Knotweed Mapping.R
@@ -1,7 +1,10 @@
 HABS <- read.csv("garlandbrook_streamsurveyCSV.csv", header=TRUE)
+BT <- read.csv("BrookTroutGB_2021_CSV.csv", header=TRUE)
 str(HABS)
+str(BT)
 
 HABS$Site_Number <- as.numeric(substr(HABS$Site,4,6)) #turn sites from text to number
+BT$BT_num <- as.numeric(substr(BT$BT_id,7,9))
 
 windows() #create a larger window to print plot
 
@@ -13,6 +16,9 @@ plot(x=HABS$Longitude, y=HABS$Latitude, xlab = "Longitude", ylab = "Latitude",ty
 points(x=HABS$Longitude[which(HABS$Knotweed_Num>0)], y=HABS$Latitude[which(HABS$Knotweed_Num>0)], col = "red", pch = 16,
        cex =sqrt(HABS$total_knotweed_area_m2)/5)
 #cex used to make size of plot point correspondent to total area of knotweed
+
+#plot where brook trout was collected
+points(x=BT$Lon,y=BT$Lat, col="green", pch=16)
 
 text(x=HABS$Longitude, y=HABS$Latitude,labels = HABS$Site_Number,cex=0.7) #make plot points habitat numbers
 
