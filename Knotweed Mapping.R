9b0065a8f702321649aa2c17a9775570d540f408
mscoots <Matt.Scott@unh.edu>
2022-07-19 18:50:08 -0400

    color change

--- Knotweed Mapping.R
+++ Knotweed Mapping.R
@@ -15,21 +15,21 @@ plot(x=HABS$Longitude, y=HABS$Latitude, xlab = "Longitude", ylab = "Latitude",ty
 #plot locations of knotweed
 kw.habs <- HABS[which(HABS$Knotweed_Num>0),]
 points(x=kw.habs$Longitude, y=kw.habs$Latitude, cex=sqrt(kw.habs$total_knotweed_area_m2/5),
-       col="red")
+       col="green")
 #cex used to make size of plot point correspondent to total area of knotweed
 
 #plot where brook trout was collected
-points(x=BT$Lon,y=BT$Lat, col="green", pch=16)
+points(x=BT$Lon,y=BT$Lat, col="red", pch=16)
 
 #plot where macroinvertebrates were collected
-points(x=macro.habs$Longitude,y=macro.habs$Latitude, col="orange", pch=16)
+points(x=macro.habs$Longitude,y=macro.habs$Latitude, col="#FF9900", pch=16)
 
 #make plot points habitat numbers
 text(x=HABS$Longitude, y=HABS$Latitude,labels = HABS$Site_Number,cex=0.7)
 
 #use xlim/ylim to zoom in on areas of the graph
 plot(x=HABS$Longitude, y=HABS$Latitude, xlab = "Longitude", ylab = "Latitude",type="n", cex=0.7,
-     xlim=c(-71.493,-71.490), ylim=c(44.465,44.471))
+     xlim=c(-71.486,-71.483), ylim=c(44.471,44.473))
 
 cbind(HABS$Site_Number,HABS$total_knotweed_area_m2)
 
